e822bf7595e4b3a76dd4adaaaf27c0014236f6d9
raefgit <48769477+raefgit@users.noreply.github.com>
2019-08-15 16:12:09 +0200

    Bundling

--- sentiment_beam.py
+++ sentiment_beam.py
@@ -1,27 +1,34 @@
 import apache_beam as beam
 import logging
+from random import randint
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
 
+PROJECT = 'elite-impact-224414'
 schema = 'name : STRING, id : STRING, date : STRING,title : STRING, text: STRING,magnitude : STRING, score : STRING'
-src_path = "gs://amazoncustreview/sentimentSource.csv"
+src_path = "gs://amazoncustreview/DatafinitiElectronicsProductData.csv"
 
 
 class Split(beam.DoFn):
-    def process(self, element):
-        element = element.split(",")
-        client = language.LanguageServiceClient()
-        document = types.Document(content=element[2],
-                                  type=enums.Document.Type.PLAIN_TEXT)
-        sentiment = client.analyze_sentiment(document).document_sentiment
-        return [{
-            'name': element[0],
-            'title': element[1],
-            'magnitude': sentiment.magnitude,
-            'score': sentiment.score
-        }]
+    def process(self, elements):
+        for element in elements:
+            element = element.split(",")
+            client = language.LanguageServiceClient()
+            document = types.Document(content=element[21],
+                                      type=enums.Document.Type.PLAIN_TEXT)
+            sentiment = client.analyze_sentiment(document).document_sentiment
+            return [{
+                'name': element[23],
+                'title': element[22],
+                'id': element[0],
+                'date': element[5],
+                'text': element[21],
+                'magnitude': str(sentiment.magnitude),
+                'score': str(sentiment.score)
+            }]
+
 
 
 def main():
@@ -31,15 +38,20 @@ def main():
       '--staging_location=gs://{0}/staging/'.format(BUCKET),
       '--temp_location=gs://{0}/staging/'.format(BUCKET),
       '--runner=DataflowRunner',
-      '--job_name=examplejob2',
-      '--save_main_session'
+      '--job_name=sentimentanalysis2',
+      '--save_main_session',
+      '--requirements_file=requirements.txt',
+      '--num_workers=1'
     ]
     p = beam.Pipeline(argv=argv)
 
     (p
        | 'ReadData' >> beam.io.textio.ReadFromText(src_path)
+       | 'AddKeys' >> beam.Map(lambda x: (randint(0, 700), x))
+       | 'MakeBundles' >> beam.GroupByKey()
+       | 'DropKeys' >> beam.Map(lambda (k, bundle): bundle)
        | 'ParseCSV' >> beam.ParDo(Split())
-       | 'WriteToBigQuery' >> beam.io.WriteToBigQuery('{0}:customer_review.customerReview2'.format(PROJECT),
+       | 'WriteToBigQuery' >> beam.io.WriteToBigQuery('{0}:customer_review.customerReviewResult'.format(PROJECT),
         write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
     )
     p.run()
